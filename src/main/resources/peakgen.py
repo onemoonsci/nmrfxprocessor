f7095dc6466c9f4df9c33c6cb92f5c6c52d3575b
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2017-12-14 12:41:58 -0500

    Initial add of peakgen.py

---
+++ src/main/resources/peakgen.py
@@ -0,0 +1,35 @@
+from org.nmrfx.processor.datasets.peaks import Peak
+from org.nmrfx.processor.datasets.peaks import PeakList
+from org.nmrfx.processor.datasets import Dataset
+
+def makePeakListFromDataset(listName,dataset):
+    nDim = dataset.getNDim()
+    peakList = PeakList(listName, nDim)
+    for i in range(nDim):
+        specDim = peakList.getSpectralDim(i)
+        specDim.setSf(dataset.getSf(i))
+        specDim.setSw(dataset.getSw(i))
+        specDim.setDimName(dataset.getLabel(i))
+    peakList.setDatasetName(dataset.getName())
+    return peakList
+
+def makePeakList(listName,labels,sfs,sws):
+    nDim = len(labels)
+    peakList = PeakList(listName, nDim)
+    for i,(label,sf,sw) in enumerate(zip(labels,sfs,sws)):
+        specDim = peakList.getSpectralDim(i)
+        specDim.setSf(sf)
+        specDim.setSw(sw)
+        specDim.setDimName(label)
+    peakList.setDatasetName("peaks")
+    return peakList
+
+def addPeak(peakList,ppms,widths,intensity,names):
+    peak = peakList.getNewPeak()
+    for i,(ppm,width,name) in enumerate(zip(ppms,widths,names)):
+        peakDim = peak.getPeakDim(i)
+        peakDim.setChemShiftValue(ppm)
+        peakDim.setLabel(name)
+        peakDim.setLineWidthValue(width)
+        peakDim.setBoundsValue(width*2.0)
+    peak.setIntensity(intensity)
