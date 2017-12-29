6bc1cc661d3051709660aa5df449fd9f255e9eb2
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2017-12-29 18:35:12 -0500

    Move Project to a new org.nmrfx.project package

--- src/main/java/org/nmrfx/project/Project.java
+++ src/main/java/org/nmrfx/project/Project.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.nmrfx.processor.project;
+package org.nmrfx.project;
 
 import java.io.File;
 import java.io.FileWriter;
