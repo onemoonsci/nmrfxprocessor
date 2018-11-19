1b866c2155e9d77ae7b3b270a0cb85a677914cdf
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2018-11-19 18:00:57 -0500

    Adjust dwell time of vectors that are extracted from datasets. Do this if they aren't same size as dimension

--- src/main/java/org/nmrfx/processor/datasets/Dataset.java
+++ src/main/java/org/nmrfx/processor/datasets/Dataset.java
@@ -4669,6 +4669,9 @@ public class Dataset extends DoubleVector implements Comparable<Dataset> {
         rwVector.resize(rwVector.getSize(), getComplex_r(dim[0]));
         rwVector.centerFreq = getSf(dim[0]);
         rwVector.dwellTime = 1.0 / getSw_r(dim[0]);
+        if (rwVector.getSize() != getSize(dim[0])) {
+            rwVector.dwellTime *= (double) getSize(dim[0]) / rwVector.getSize();
+        }
         rwVector.setPh0(getPh0_r(dim[0]));
         rwVector.setPh1(getPh1_r(dim[0]));
         rwVector.setTDSize(getTDSize(dim[0]));
