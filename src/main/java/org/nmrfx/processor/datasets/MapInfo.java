5f9ed478ded8ea52d626a032e5040e9561cb5e92
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-01-30 17:09:16 -0500

    Add new buffer cleaner that should work with newer Java (9...)

--- src/main/java/org/nmrfx/processor/datasets/MapInfo.java
+++ src/main/java/org/nmrfx/processor/datasets/MapInfo.java
@@ -19,14 +19,16 @@ package org.nmrfx.processor.datasets;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Collections;
 import java.util.Map;
 import org.apache.commons.collections4.map.*;
-import sun.misc.Cleaner;
-import sun.nio.ch.DirectBuffer;
 
 public class MapInfo {
 
@@ -128,11 +130,48 @@ public class MapInfo {
         if (mapMap != null) {
             mapMap.remove(this);
         }
-        Cleaner cleaner = ((DirectBuffer) buffer).cleaner();
-        if (cleaner != null) {
-            cleaner.clean();
-        }
+        closeDirectBuffer(buffer);
         buffer = null;
         nMaps--;
+    }
+
+    // code from 
+    // https://stackoverflow.com/questions/2972986/
+    // how-to-unmap-a-file-from-memory-mapped-using-filechannel-in-java
+    public static void closeDirectBuffer(ByteBuffer cb) {
+        if (cb == null || !cb.isDirect()) {
+            return;
+        }
+        // we could use this type cast and call functions without reflection code,
+        // but static import from sun.* package is risky for non-SUN virtual machine.
+        //try { ((sun.nio.ch.DirectBuffer)cb).cleaner().clean(); } catch (Exception ex) { }
+
+        // JavaSpecVer: 1.6, 1.7, 1.8, 9, 10
+        boolean isOldJDK = System.getProperty("java.specification.version", "99").startsWith("1.");
+        try {
+            if (isOldJDK) {
+                Method cleaner = cb.getClass().getMethod("cleaner");
+                cleaner.setAccessible(true);
+                Method clean = Class.forName("sun.misc.Cleaner").getMethod("clean");
+                clean.setAccessible(true);
+                clean.invoke(cleaner.invoke(cb));
+            } else {
+                Class unsafeClass;
+                try {
+                    unsafeClass = Class.forName("sun.misc.Unsafe");
+                } catch (ClassNotFoundException ex) {
+                    // jdk.internal.misc.Unsafe doesn't yet have an invokeCleaner() method,
+                    // but that method should be added if sun.misc.Unsafe is removed.
+                    unsafeClass = Class.forName("jdk.internal.misc.Unsafe");
+                }
+                Method clean = unsafeClass.getMethod("invokeCleaner", ByteBuffer.class);
+                clean.setAccessible(true);
+                Field theUnsafeField = unsafeClass.getDeclaredField("theUnsafe");
+                theUnsafeField.setAccessible(true);
+                Object theUnsafe = theUnsafeField.get(null);
+                clean.invoke(theUnsafe, cb);
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException ex) {
+        }
     }
 }
