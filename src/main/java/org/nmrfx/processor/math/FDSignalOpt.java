696904e89af80561dcc48c8d3e4226c6ceb7e30f
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-01-31 15:07:54 -0500

    Cleanup based on hints

--- src/main/java/org/nmrfx/processor/math/FDSignalOpt.java
+++ src/main/java/org/nmrfx/processor/math/FDSignalOpt.java
@@ -25,7 +25,6 @@ import org.apache.commons.math3.optim.SimpleBounds;
 import org.apache.commons.math3.optim.InitialGuess;
 import org.apache.commons.math3.optim.nonlinear.scalar.ObjectiveFunction;
 import org.apache.commons.math3.optim.MaxEval;
-import org.apache.commons.math3.util.FastMath;
 import org.apache.commons.math3.optim.PointValuePair;
 import org.apache.commons.math3.analysis.MultivariateFunction;
 import org.apache.commons.math3.exception.TooManyEvaluationsException;
@@ -52,13 +51,13 @@ public class FDSignalOpt implements MultivariateFunction {
     private double finalValue = 0.0;
     private double finalDelta = 0.0;
     private double[] weights;
-    private int[] parMap;
+    private final int[] parMap;
 
     public FDSignalOpt(double[] testVec, int vecSize, ArrayList<Signal> signals, boolean constrainWidth, int leftEdge, int rightEdge) {
         this.testVec = testVec;
         this.values = new double[vecSize];
         this.vecSize = vecSize;
-        this.signals = new ArrayList<Signal>();
+        this.signals = new ArrayList<>();
         this.parMap = new int[3 * signals.size()];
         this.leftEdge = leftEdge;
         this.rightEdge = rightEdge;
@@ -197,7 +196,7 @@ public class FDSignalOpt implements MultivariateFunction {
         return rss;
     }
 
-    public void calcWeights() {
+    public final void calcWeights() {
         weights = new double[testVec.length];
         for (int i = 0; i < weights.length; i++) {
             weights[i] = 1.0;
@@ -240,6 +239,7 @@ public class FDSignalOpt implements MultivariateFunction {
         return maxDelta;
     }
 
+    @Override
     public double value(final double[] opars) {
         denormalize(opars, parameters);
         toSignal(parameters);
@@ -268,7 +268,7 @@ public class FDSignalOpt implements MultivariateFunction {
 
         normalize(parameters, normValues);
 
-        PointValuePair result = null;
+        PointValuePair result;
         try {
             result = optimizer.optimize(
                     new MaxEval(nSteps),
