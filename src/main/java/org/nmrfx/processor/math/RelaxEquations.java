f830c2e5c21f1454dca42fa56789320dbcb496b6
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-04-29 13:21:29 -0400

    Cleanup based on hints

--- src/main/java/org/nmrfx/processor/math/RelaxEquations.java
+++ src/main/java/org/nmrfx/processor/math/RelaxEquations.java
@@ -13,7 +13,7 @@ public class RelaxEquations {
     public final static double H_BAR = H / (2.0 * Math.PI);
     public final static double SQRT2 = Math.sqrt(2.0);
 
-    double r = 1.02e-10;
+    private static final double r = 1.02e-10;
     // don't use this yet.  Various inconsistencies with various different presentations of equations
     //   consider using scaled versions (smaller exponents)
 
@@ -28,17 +28,17 @@ public class RelaxEquations {
         return value;
     }
 
-    public double J(double w, double tau) {
+    public static double J(double w, double tau) {
         double value = 0.4 * tau / (1.0 + w * w * tau * tau);
         return value;
     }
 
-    public double J0(double tau) {
+    public static double J0(double tau) {
         double value = 0.4 * tau;
         return value;
     }
 
-    public double T1(double tau) {
+    public static double T1(double tau) {
         double wH = 800.0e6 * 2.0 * Math.PI;
         double wN = Math.abs(wH * GAMMA_N / GAMMA_H);
         double A = (J(wH - wN, tau) + 3.0 * J(wN, tau) + 6.0 * J(wH + wN, tau));
@@ -50,7 +50,7 @@ public class RelaxEquations {
 
     }
 
-    public double T2(double tau) {
+    public static double T2(double tau) {
         double wH = 800.0e6 * 2.0 * Math.PI;
         double wN = Math.abs(wH * GAMMA_N / GAMMA_H);
         double A = (4.0 * J(0.0, tau) + J(wH - wN, tau) + 3.0 * J(wN, tau) + 6.0 * J(wH, tau) + 6.0 * J(wH + wN, tau));
@@ -62,14 +62,14 @@ public class RelaxEquations {
 
     }
 
-    public double T2s(double tau) {
+    public static double T2s(double tau) {
         double A = (1.0 / Math.pow((2.0 * Math.PI), 2)) * Math.pow(MU0 / (4.0 * Math.PI), 2);
         double x = Math.pow(GAMMA_N * GAMMA_H * H_BAR, 2) / Math.pow(r, 6);
         double invT2 = A * x * tau;
         return invT2;
     }
 
-    public double TRACTdeltaAlphaBeta(double B0, double tauC) {
+    public static double TRACTdeltaAlphaBeta(double B0, double tauC) {
         double ddN = 160.0e-6;
         double theta = 17.0 * Math.PI / 180.0;
         double p = MU0 * GAMMA_H * GAMMA_N * H / (16.0 * Math.PI * Math.PI * SQRT2 * r * r * r);
