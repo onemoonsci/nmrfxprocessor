e388f6ec9f3325f4fdd3647832676accfb041e75
tedcolon <tedcolon@guanine.asrc.cuny.edu>
2019-10-31 17:01:16 -0400

    Created a file to calculate the Hausdroff distance between collections

---
+++ src/main/java/org/nmrfx/processor/optimization/HausdroffDistance.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nmrfx.processor.optimization;
+
+import java.util.*;
+
+/**
+ *
+ * @author tedcolon
+ */
+public class HausdroffDistance {
+
+    private final Set<Collection> setA;
+    private final Set<Collection> setB;
+
+    public HausdroffDistance(Set<Collection> setA, Set<Collection> setB) {
+        this.setA = setA;
+        this.setB = setB;
+    }
+
+    public static Double distance(Collection ptA, Collection ptB) throws IllegalArgumentException {
+        if (ptA.size() != ptB.size()) {
+            throw new IllegalArgumentException(String.format("Collection sizes do not match. (%d != %d)", ptA.size(), ptB.size()));
+        }
+        Iterator<Double> aCoor = ptA.iterator(); // coordinates for ptA, i.e. (X1, Y1, Z1)
+        Iterator<Double> bCoor = ptB.iterator(); // coordinates for ptB, i.e. (X2, Y2, Z2)
+        double sum = 0.0;
+        double diff;
+        while (aCoor.hasNext() && bCoor.hasNext()) {
+            diff = bCoor.next() - aCoor.next();
+            sum += Math.pow(diff, 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    public Double calculate() throws IllegalArgumentException {
+        // brute force algorithm to calculate Hausdroff distance b/t sets A,B
+        // - find minimum distances for each pt from A(i) to all B(j) (asymmetric/unidirectional).
+        // - find the maximum distance out of all minimum distances calculated.
+        if (setA.isEmpty() || setB.isEmpty()) {
+            throw new IllegalArgumentException("Empty sets are invalid.");
+        }
+        Double finalDist = 0.0;
+        Double shortest;
+        Double curDist;
+        for (Collection ai : setA) {
+            shortest = Double.POSITIVE_INFINITY;
+            for (Collection bj : setB) {
+                curDist = distance(ai, bj);
+                if (curDist < shortest) {
+                    shortest = curDist;
+                }
+            }
+            if (shortest > finalDist) {
+                finalDist = shortest;
+            }
+        }
+        return finalDist;
+    }
+}
