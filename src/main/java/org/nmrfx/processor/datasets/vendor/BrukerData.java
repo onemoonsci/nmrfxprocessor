03bd6cf5b3e9a18f285e6bb1ffa46fbf5cc1f1bf
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2019-12-05 16:54:00 -0500

    Allow "m" character in vdlist (interpret as milliseconds

--- src/main/java/org/nmrfx/processor/datasets/vendor/BrukerData.java
+++ src/main/java/org/nmrfx/processor/datasets/vendor/BrukerData.java
@@ -870,7 +870,15 @@ public class BrukerData implements NMRData {
 
                         for (String sValue : sList) {
                             try {
-                                result.add(Double.parseDouble(sValue));
+                                double scale = 1.0;
+                                if (sValue.endsWith("m)")) {
+                                    sValue = sValue.substring(0, sValue.length() - 1);
+                                    scale = 1.0e-3;
+                                } else if (sValue.endsWith("s")) {
+                                    sValue = sValue.substring(0, sValue.length() - 1);
+                                    scale = 1.0;
+                                }
+                                result.add(Double.parseDouble(sValue) * scale);
                             } catch (NumberFormatException nFE) {
                                 System.out.println("bad double " + sValue);
                                 result = null;
@@ -881,7 +889,9 @@ public class BrukerData implements NMRData {
                     }
                 }
             }
-            result = fixArraySize(result, smallDim);
+            if (result != null) {
+                result = fixArraySize(result, smallDim);
+            }
         }
         return result;
     }
