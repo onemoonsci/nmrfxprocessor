c34dacb558d00357d5e2d54aafd26e278f934368
Bruce Johnson <brucejohnson@localhost>
2019-12-29 11:57:02 -0500

    Add copyList to copy list of components (possibly skipping one)

--- src/main/java/org/nmrfx/processor/datasets/peaks/AbsMultipletComponent.java
+++ src/main/java/org/nmrfx/processor/datasets/peaks/AbsMultipletComponent.java
@@ -1,5 +1,7 @@
 package org.nmrfx.processor.datasets.peaks;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.nmrfx.processor.datasets.Dataset;
 
 /**
@@ -40,6 +42,25 @@ public class AbsMultipletComponent extends MultipletComponent {
 
         width[0] = Math.abs(p[0][0] - p[0][1]);
 
+    }
+
+    public static List<AbsMultipletComponent> copyList(List<AbsMultipletComponent> comps) {
+        return copyList(comps, -1);
+    }
+
+    public static List<AbsMultipletComponent> copyList(List<AbsMultipletComponent> comps, int skip) {
+        List<AbsMultipletComponent> newList = new ArrayList<>();
+        for (int i = 0; i < comps.size(); i++) {
+            if (i != skip) {
+                AbsMultipletComponent oldComp = comps.get(i);
+                AbsMultipletComponent newComp = new AbsMultipletComponent(
+                        oldComp.multiplet, oldComp.offset, oldComp.intensity,
+                        oldComp.volume, oldComp.lineWidth);
+                newList.add(newComp);
+            }
+        }
+
+        return newList;
     }
 
 }
