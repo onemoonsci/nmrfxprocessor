c7ed96e1c64d236dec18cd2b23eaabcd18324f1c
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2020-03-11 20:48:39 -0400

    Add nuscon methods to pscript

--- src/main/resources/pscript.py
+++ src/main/resources/pscript.py
@@ -3,6 +3,7 @@ from org.nmrfx.processor.datasets.peaks import PeakList
 from org.nmrfx.processor.datasets.peaks import Peak
 from org.nmrfx.processor.datasets.peaks.io import PeakReader
 from org.nmrfx.processor.datasets.peaks.io import PeakWriter
+from org.nmrfx.processor.datasets.peaks import NUSConScore
 
 class NMRFxPeakScripting:
 
@@ -22,5 +23,30 @@ class NMRFxPeakScripting:
     def write(self, peakList, fileName):
         PeakWriter.writePeaksXPK2(fileName, peakList)
 
+    def nuscon(self, peakListName1, peakListName2, dMax = 4.0):
+        peakList1 = self.get(peakListName1)
+        peakList2 = self.get(peakListName2)
+        if peakList1 == None:
+            raise "No peakList "+peakListName1
+        if peakList2 == None:
+            raise "No peakList "+peakListName2
+     
+        nusConScore = NUSConScore(peakList1, peakList2)
+        nusConScore.calculate(dMax)
+        accuracy = nusConScore.getAccuracy()
+        linearity = nusConScore.getLinearity()
+        truePositiveRate = nusConScore.getTruePositiveRate()
+        falsePositiveRate = nusConScore.getFalsePositiveRate()
+        result = {"accuracy":accuracy, "linearity":linearity, "truePos":truePositiveRate, "falsePos": falsePositiveRate}
+        return result
+  
+
+    def nusconValley(self, dataset, peakSpec1, peakSpec2, iDim):
+        peak1 = PeakList.getAPeak(peakSpec1)
+        peak2 = PeakList.getAPeak(peakSpec2)
+        score = NUSConScore.valleyToPeak(dataset, peak1,peak2, iDim)
+        return score
+    
+        
 npk = NMRFxPeakScripting()
 
